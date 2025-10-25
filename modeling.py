3958777b6cadc28d025df6aa4b4bf8a5fbd9ff91
Nitish <nitishpandey2117@gmail.com>
2025-10-25 18:24:47 +0530

    update forward pass in modeling.py

--- modeling.py
+++ modeling.py
@@ -15,11 +15,11 @@ class MLPLayer(nn.Module):
         super().__init__()
         self.config = config
         self.up_proj = nn.Linear(config.hidden_dim, 4 * config.hidden_dim)
+        self.act_fn = nn.ReLU()
         self.down_proj = nn.Linear(4 * config.hidden_dim, config.hidden_dim)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        pass
-
+        return self.down_proj(self.act_fn(self.up_proj(inputs)))
 
 
 class AttentionLayer(nn.Module):
@@ -32,8 +32,19 @@ class AttentionLayer(nn.Module):
         self.out_proj = nn.Linear(config.hidden_dim, config.hidden_dim)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        pass
-
+        query = self.q_proj(inputs)
+        key = self.k_proj(inputs)
+        value = self.v_proj(inputs)
+        x = F.scaled_dot_product_attention(
+            query,
+            key,
+            value,
+            dropout_p=0.1,
+            is_causal=True,
+            scale=self.config.hidden_dim**-0.5
+        )
+        out = self.out_proj(x)
+        return out
 
 
 class DecoderLayer(nn.Module):
@@ -46,8 +57,13 @@ class DecoderLayer(nn.Module):
         self.mlp_layer = MLPLayer(config)
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        pass
-
+        res = inputs
+        x = self.attn_layer(self.pre_attn_norm(res))
+        x += res
+        res = x
+        x = self.mlp_layer(self.pre_mlp_norm(res))
+        x += res
+        return x
 
 
 class MicroLLM(nn.Module):
@@ -55,11 +71,23 @@ class MicroLLM(nn.Module):
         super().__init__()
         self.config = config
 
-        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_dim)
+        self.embed = nn.Embedding(config.vocab_size, config.hidden_dim)
         self.decoder_layers = nn.ModuleList([
             DecoderLayer(config) for _ in range(config.num_layers)
         ])
+        self.final_norm = nn.RMSNorm(config.hidden_dim)
         self.lm_head = nn.Linear(config.hidden_dim, config.vocab_size)
 
     def forward(self, input_ids: torch.Tensor) -> torch.Tensor:
-        pass
+        x = self.embed(input_ids)
+        for layer in self.decoder_layers:
+            x = layer(x)
+        x = self.final_norm(x)
+        logits = self.lm_head(x)
+        return logits
+
+
+
+
+
+
